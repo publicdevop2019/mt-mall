48f72e95245dce82b1865aef52b5d120223807be
publicdevop2019 <publicdevop2019hw@gmail.com>
2022-02-19 16:46:35 -0500

    update project description and local env

--- mt-ui-admin/src/environments/environment.ts
+++ mt-ui-admin/src/environments/environment.ts
@@ -10,9 +10,9 @@ export const environment = {
   CLIENT_ID: '0C8HQM52YN7K',
   PROJECT_ID: '0P8HPG99R56P',
   CLIENT_SECRET_PUBLIC: '',
-  AUTHORIZATION_URL: 'http://localhost:4300/authorize?response_type=code&',
+  AUTHORIZATION_URL: 'https://admin.duoshu.org/authorize?response_type=code&',
   OAUTH2_REDIRECT_URL: 'http://localhost:4400',
-  TOKEN_URL: 'http://localhost:4400/proxy/auth-svc/oauth/token',
+  TOKEN_URL: 'https://api.duoshu.org/auth-svc/oauth/token',
 };
 
 /*
